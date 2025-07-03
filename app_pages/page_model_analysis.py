b11833a5b6087c4afcd071f569fcb4a50bd1db31
Emily Wade <57637958+emilywade@users.noreply.github.com>
2025-07-03 19:20:36 +0000

    adding insights and performance outputs to model analysis page

--- app_pages/page_model_analysis.py
+++ app_pages/page_model_analysis.py
@@ -23,6 +23,23 @@ def page_model_analysis():
     st.write("### ML Pipeline: Predict House Price")
 
     st.write("---")
+    
+    st.info(
+        f"Initially we considered various regression models including linear regression"
+        f" methods and ensemble tree-based approaches to predict house sale prices. We started"
+        f" by training an ElasticNet model, but on evaluating the performance it was clear the"
+        f" skew in sale price was not being handled. \n\n"
+        f"After handling target imbalance, LinearRegression and Ridge models performed the best."
+        f" We decided to opt for a Ridge model due to its regularisation capabilities. \n\n"
+        f"We used GridSearch CV to optimise the Ride regression hyperparameters. The model achieved"
+        f"reasonable performance, although it is important to acknowledge that potential feature "
+        f"engineering limitations (eg. missing data) remains a challenge that can affect prediction "
+        f"quality. "
+    )
+    
+    st.write("---")
+
+
 
     # show pipeline steps
     st.write("* ML pipeline to predict house prices")
@@ -34,3 +51,15 @@ def page_model_analysis():
     st.write(X_train.columns.to_list())
     st.image(price_feat_importance)
     st.write("---")
+
+    eval_text_path = f"outputs/ml_pipeline/predict_sale_price/{version}/regression_performance.txt"
+    with open(eval_text_path, "r") as file:
+        eval_text = file.read()
+    st.write("### Regression Evaluation Metrics")
+    st.code(eval_text)
+
+    st.write("---")
+
+    eval_plot_path = f"outputs/ml_pipeline/predict_sale_price/{version}/regression_evaluation_plots.png"
+    st.write("### Regression Evaluation Plots")
+    st.image(eval_plot_path)
