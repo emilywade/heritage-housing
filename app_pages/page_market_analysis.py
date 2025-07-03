d373172761109553630e4384ce04b8bee8763310
Emily Wade <57637958+emilywade@users.noreply.github.com>
2025-07-03 14:47:11 +0000

    adding cleaned dataset checkbox to second page

--- app_pages/page_market_analysis.py
+++ app_pages/page_market_analysis.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.data_management import load_housing_data
+from src.data_management import load_original_housing_data, load_cleaned_housing_data
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
@@ -9,10 +9,11 @@ import plotly.express as px
 def page_market_analysis_body():
 
     # load data
-    df = load_housing_data()
+    df_original = load_original_housing_data()
+    df_cleaned = load_cleaned_housing_data()
 
     # variables found relevant during EDA
-    vars_to_study = ['OverallQual', 'TotalSF', 'KitchenQual', 'GrLivArea', 'GarageArea', 'HouseAge', 'RemodAge']
+    vars_to_study = ['AboveGradeSF', 'GarageArea', 'GrLivArea', 'HouseAge', 'OverallQual', 'TotalSF']
 
     st.write("### House Price Study")
     st.info(
@@ -21,10 +22,35 @@ def page_market_analysis_body():
     )
 
     # inspect data
-    if st.checkbox("Inspect Dataset"):
+    if st.checkbox("Inspect Original Dataset"):
         st.write(
-            f"* The dataset has {df.shape[0]} rows and {df.shape[1]} columns. "
+            f"* The dataset has {df_original.shape[0]} rows and {df_original.shape[1]} columns. "
             f"Below are the first 10 rows:")
-        st.write(df.head(10))
+        st.write(df_original.head(10))
 
     st.write("---")
+
+    st.info(
+        f"The dataset required some initial cleaning before investigating  "
+        f"correlations between variables.\n"
+        f"Below is the cleaned dataset after:\n"
+        f"* Dropping WoodDeckSF and EnclosedPorch due to severe levels of missing data\n"
+        f"* Imputing missing values in LotFrontage, BedroomAbvGr and MasVnrArea with median values\n"
+        f"* Imputing missing values in GarageFinish, BsmtFinType1 and BsmtExposure with 'missing'\n"
+    )
+
+    # inspect cleaned dataset
+    if st.checkbox("Inspect Cleaned Dataset"):
+        st.write(
+            f"* The dataset has {df_cleaned.shape[0]} rows and {df_cleaned.shape[1]} columns. "
+            f"Below are the first 10 rows:")
+        st.write(df_cleaned.head(10))
+
+    st.write("---")
+
+    # correlation study summary
+    st.write(
+        f"* An exploratory analysis was conducted to study correlations with SalePrice. \n"
+        f"The variables most strongly correlated with SalePrice are: **{vars_to_study}**."
+    )
+
--- src/data_management.py
+++ src/data_management.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas as pd
 
 @st.cache_data
-def load_housing_data():
+def load_original_housing_data():
     df = pd.read_csv("outputs/datasets/collection/HousingPrices.csv")
     return df
+
+def load_cleaned_housing_data():
+    df = pd.read_csv("outputs/datasets/cleaned/HousingPrices.csv")
+    return df
+
+     
