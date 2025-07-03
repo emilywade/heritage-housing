f0782a5069d7457aada73ad0fe4137c847b9b7b1
Emily Wade <57637958+emilywade@users.noreply.github.com>
2025-07-03 16:57:30 +0000

    adding ml pipeline evaluation page

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_market_analysis import page_market_analysis_body
 from app_pages.page_predict import page_house_price_predictor_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_model_analysis import page_model_analysis
 
 app = MultiPage(app_name="Sale Price Predictor")
 
@@ -12,5 +13,6 @@ app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Housing Prices Market Analysis", page_market_analysis_body)
 app.add_page("Housing Price Predictor", page_house_price_predictor_body)
 app.add_page("Project Hypotheses", page_project_hypothesis_body)
+app.add_page("ML Pipeline: Housing Price Predictor", page_model_analysis)
 
 app.run()
---
+++ app_pages/page_model_analysis.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def page_model_analysis():
+    return ""
