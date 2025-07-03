e8e3eeb1a6ac87f9b9ef0185b7ff31331ed462a1
Emily Wade <57637958+emilywade@users.noreply.github.com>
2025-07-03 18:09:40 +0000

    adding pipeline and best features to model analysis page

--- app_pages/page_model_analysis.py
+++ app_pages/page_model_analysis.py
@@ -1,4 +1,36 @@
 import streamlit as st
+import pandas as pd
+from src.data_management import load_pkl_file
+import matplotlib.pyplot as plt
 
 def page_model_analysis():
-    return ""
+    version = 'v1'
+    pipe = load_pkl_file(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/reg_pipeline.pkl")
+    tenure_labels_map = load_pkl_file(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/mappings.pkl")
+    price_feat_importance = plt.imread(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/features_importance.png")
+    X_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/X_train.csv")
+    X_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/X_test.csv")
+    y_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/y_train.csv")
+    y_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/y_test.csv")
+
+    st.write("### ML Pipeline: Predict House Price")
+
+    st.write("---")
+
+    # show pipeline steps
+    st.write("* ML pipeline to predict house prices")
+    st.write(pipe)
+    st.write("---")
+
+    # show best features
+    st.write("* The features the model was trained and their importance.")
+    st.write(X_train.columns.to_list())
+    st.image(price_feat_importance)
+    st.write("---")
